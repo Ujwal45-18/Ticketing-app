a651645d7798df20d40aefa9d5cd6c2268c0671d
Ujwal45-18 <9eujwal@gmail.com>
2025-08-22 21:05:55 +0530

    changed redirection from localhost

--- app/TicketPage/[id]/page.jsx
+++ app/TicketPage/[id]/page.jsx
@@ -2,7 +2,7 @@ import EditTicketForm from "@/app/(components)/EditTicketForm";
 
 const getTicketById = async (id) => {
   try {
-    const res = await fetch(`http://localhost:3000/api/Tickets/${id}`, {
+    const res = await fetch(`http://ticketing-8islpur2q-ujwal-bs-projects.vercel.app/api/Tickets/${id}`, {
       cache: "no-store",
     });
 
